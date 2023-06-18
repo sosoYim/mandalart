d381a51de3dfb9989921f4b8efb94fcb803f2c02
sosoYim <sohyung.yim@gmail.com>
2023-06-18 14:16:23 +0900

    chore: 프리즈마 업데이트 및 설정
    - 프리티어
    - nvmrc 추가

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,10 @@
 /** @type {import("prettier").Config} */
 const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
-};
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+}
 
-module.exports = config;
+module.exports = config
